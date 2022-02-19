9d23b74b890587c4a08327f7071cddfba00e8ae9
Xu Ma <xuma@my.unt.edu>
2022-02-18 22:35:15 -0500

    update

--- mmseg/models/uda/__init__.py
+++ mmseg/models/uda/__init__.py
@@ -1,3 +1,5 @@
 from mmseg.models.uda.dacs import DACS
+from mmseg.models.uda.dacs_nofdist import DACSNOFDIST
+from mmseg.models.uda.dacs_nomix import DACSNOMIX
 
-__all__ = ['DACS']
+__all__ = ['DACS', "DACSNOFDIST", "DACSNOMIX"]
